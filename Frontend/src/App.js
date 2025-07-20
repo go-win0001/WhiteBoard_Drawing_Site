459b91e703cdfbb231c874816ceb34222f9954e6
hacker <gs6055363@gmail.com>
2025-07-20 17:57:22 +0530

    login page bugFixed(light)

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
    <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/load/:id" element={<Canvas />} />
       </Routes>
